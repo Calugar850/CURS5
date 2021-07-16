010b332d2e2443d0ebdfdfbae9a7192b6951e672
Calugar850 <72302669+Calugar850@users.noreply.github.com>
2021-07-16 13:02:46 +0300

    Update Tema.py

--- Tema.py
+++ Tema.py
@@ -5,46 +5,46 @@ class Fractie:
         self.numitor=numitor
 
     def __str__(self):
-        print('Fractia este: ',self.numarator,'/',self.numitor)
+        value=str(self.numarator)
+        value+='/'
+        value+=str(self.numitor)
+        return value
 
-    def __add__(self, first_numarator, first_numitor, second_numarator, second_numitor):
-        final_fraction=Fractie(1,1)
-        if first_numitor==second_numitor:
-            print('sdsad')
-            final_fraction.numarator=first_numarator+second_numarator
-            final_fraction.numitor=first_numitor
-        else:
-            final_fraction.numarator=(first_numarator*second_numitor)+(second_numarator*first_numitor)
-            final_fraction.numitor=first_numitor*second_numitor
-        if final_fraction.numarator%2==0 and final_fraction.numitor%2==0:
-            final_fraction.numarator=final_fraction.numarator/2
-            final_fraction.numitor=final_fraction.numitor/2
-        if final_fraction.numarator % 3 == 0 and final_fraction.numitor % 3 == 0:
-            final_fraction.numarator = final_fraction.numarator / 3
-            final_fraction.numitor = final_fraction.numitor / 3
-        if final_fraction.numarator%5==0 and final_fraction.numitor%5==0:
-            final_fraction.numarator=final_fraction.numarator/5
-            final_fraction.numitor=final_fraction.numitor/5
-        return final_fraction
+    def __add__(self, other):
+        if self.numitor == other.numitor:
+            new_fr = Fractie(self.numarator + other.numarator, self.numitor)
+            return new_fr
+        new_num = (other.numitor * self.numarator) + (other.numarator * self.numitor)
+        new_den = self.numitor * other.numitor
+        new_fr = Fractie(new_num, new_den)
+        if new_fr.numarator%2==0 and new_fr.numitor%2==0:
+            new_fr.numarator=new_fr.numarator/2
+            new_fr.numitor=new_fr.numitor/2
+        if new_fr.numarator%3==0 and new_fr.numitor%3==0:
+            new_fr.numarator=new_fr.numarator/3
+            new_fr.numitor=new_fr.numitor/3
+        if new_fr.numarator%5==0 and new_fr.numitor%5==0:
+            new_fr.numarator=new_fr.numarator/5
+            new_fr.numitor=new_fr.numitor/5
+        return new_fr
 
-    def __sub__(self, first_numarator, first_numitor, second_numarator, second_numitor):
-        final_fraction = Fractie(1,1)
-        if first_numitor == second_numitor:
-            final_fraction.numarator = first_numarator - second_numarator
-            final_fraction.numitor = first_numitor
-        else:
-            final_fraction.numarator = (first_numarator * second_numitor) - (second_numarator * first_numitor)
-            final_fraction.numitor = first_numitor * second_numitor
-        if final_fraction.numarator % 2 == 0 and final_fraction.numitor % 2 == 0:
-            final_fraction.numarator = final_fraction.numarator / 2
-            final_fraction.numitor = final_fraction.numitor / 2
-        if final_fraction.numarator % 3 == 0 and final_fraction.numitor % 3 == 0:
-            final_fraction.numarator = final_fraction.numarator / 3
-            final_fraction.numitor = final_fraction.numitor / 3
-        if final_fraction.numarator % 5 == 0 and final_fraction.numitor % 5 == 0:
-            final_fraction.numarator = final_fraction.numarator / 5
-            final_fraction.numitor = final_fraction.numitor / 5
-        return final_fraction
+    def __sub__(self, other):
+        if self.numitor == other.numitor:
+            new_fr = Fractie(self.numarator - other.numarator, self.numitor)
+            return new_fr
+        new_num = (other.numitor * self.numarator) - (other.numarator * self.numitor)
+        new_den = self.numitor * other.numitor
+        new_fr = Fractie(new_num, new_den)
+        if new_fr.numarator%2==0 and new_fr.numitor%2==0:
+            new_fr.numarator=new_fr.numarator/2
+            new_fr.numitor=new_fr.numitor/2
+        if new_fr.numarator%3==0 and new_fr.numitor%3==0:
+            new_fr.numarator=new_fr.numarator/3
+            new_fr.numitor=new_fr.numitor/3
+        if new_fr.numarator%5==0 and new_fr.numitor%5==0:
+            new_fr.numarator=new_fr.numarator/5
+            new_fr.numitor=new_fr.numitor/5
+        return new_fr
 
     def __inverse__(self):
         aux=self.numarator
@@ -52,19 +52,13 @@ class Fractie:
         self.numitor=aux
         return self
 
-first_fraction=Fractie(1,2)
-second_fraction=Fractie(3,4)
-sum=Fractie(1,1)
-dif=Fractie(1,1)
-inverse=Fractie(1,1)
-first_fraction.__str__()
-second_fraction.__str__()
-sum=Fractie.__add__(sum,first_fraction.numarator,first_fraction.numitor,second_fraction.numarator,second_fraction.numitor)
-dif=Fractie.__sub__(dif,first_fraction.numarator,first_fraction.numitor,second_fraction.numarator,second_fraction.numitor)
-inverse=Fractie.__inverse__(first_fraction)
-print('Sum:')
-sum.__str__()
-print('Dif:')
-dif.__str__()
-print('Inverse:')
-inverse.__str__()
+first_fraction=Fractie(1, 2)
+second_fraction=Fractie(3, 4)
+sum_fraction=first_fraction + second_fraction
+dif_fraction=first_fraction - second_fraction
+inverse_fraction=first_fraction.__inverse__()
+print('First fraction',first_fraction.__str__())
+print('Second fraction',second_fraction.__str__())
+print('Sum fraction',sum_fraction.__str__())
+print('Dif fraction',dif_fraction.__str__())
+print('Inverse fraction',inverse_fraction.__str__())
